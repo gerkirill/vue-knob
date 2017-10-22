0012d9798c3c975abaf6c50ff1db282149fff59b
kyrylo gerasymenko <kyrylo.gerasymenko@globallogic.com>
2017-10-22 22:31:02 +0300

    first steps

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <Knob state="off" @stateChange="stateChange"></Knob>
+  </div>
+</template>
+
+<script>
+import Knob from '@/components/Knob.vue'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    stateChange: function (state) {
+      console.log(state)
+    }
+  },
+  components: {
+    Knob
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Knob.vue
@@ -0,0 +1,44 @@
+<template>
+  <button @click="onClick" :class="theme">
+     {{ currentState }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Knob',
+  props: {
+    state: {
+      type: String,
+      default: 'on'
+    }
+  },
+  data () {
+    return {
+      currentState: this.state
+    }
+  },
+  computed: {
+    theme: function () {
+      return this.currentState === 'on' ? '' : 'dark'
+    }
+  },
+  methods: {
+    onClick: function (event) {
+      this.currentState = ['on', 'off'].find(_ => _ !== this.currentState)
+      this.$emit('stateChange', this.currentState)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  button {
+    font-size: 30px;
+    padding: 4px 16px;
+  }
+  button.dark {
+    background-color: #000;
+    color: #fff;
+  }
+</style>
